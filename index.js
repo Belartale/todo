90d3daa2d90158570c251adc7fab86ea6e57f2a2
belartale <belartale@gmail.com>
2021-04-22 19:15:47 +0300

    add pug

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require("express");
 const app = express();
-const mongodb = require("mongodb");
+const { MongoClient } = require("mongodb");
 const chalk = require("chalk");
+
+const Routers = require("chalk");
+
+// const fs = require("fs");
+// const path = require("path");
+
+const client = new MongoClient(
+  "mongodb+srv://admin-app-todo1:admin-app-todo1@cluster0.7h1fx.mongodb.net/app-todo?retryWrites=true&w=majority"
+);
 
 const PORT = process.env.PORT || 3000;
 
-function start(params) {
+app.set("view engine", "pug");
+
+const start = (params) => {
   try {
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
       console.log(chalk.blue("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! START SERVER"));
+
+      await client.connect();
+      // await client.db().createCollection("todos");
+      const todos = client.db().collection("todos");
+
+      // await todos.insertOne({ name: "1111111111" });
+
+      const todo = await todos.find().toArray((err, result) => {
+        console.log(result);
+
+        app.get("/", function (req, res) {
+          res.render("index", {
+            title: "Hey",
+            message: result[0].name,
+            arr: result,
+          });
+        });
+      });
+
+      // console.log(todo);
     });
   } catch (error) {
     console.log(chalk.red(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ${error}`));
   }
-}
+};
 
 start();
