aa72a8d2e20f444480b8fe90e50cefcf2c954fa6
belartale <belartale@gmail.com>
2021-04-28 16:12:47 +0300

    save

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const chalk = require("chalk");
+const fs = require("fs");
 const path = require("path");
+
+const sass = require("node-sass");
 
 const todosRouters = require("./routers/todos");
 
@@ -9,14 +12,20 @@ const PORT = process.env.PORT || 3000;
 
 //
 
+app.set("view engine", "sass");
+
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(todosRouters);
 
 const start = (params) => {
   try {
-    app.listen(PORT, async () => {
+    app.listen(PORT, () => {
       console.log(chalk.blue("START SERVER"));
+      //!
+
+      //!
     });
   } catch (error) {
     console.log(chalk.red(error));
